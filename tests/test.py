085843d5b5afe62d17f3f7f66613ed38742d9847
Eamon Burns (Laptop WSL) <me@eamonburns.com>
2024-06-09 11:52:35 -0700

    feat: add tests

---
+++ tests/test.py
@@ -0,0 +1,17 @@
+from form_tui import Field
+import unittest
+
+
+class FieldTest(unittest.TestCase):
+    def test_validate(self):
+        without_validator = Field("email", "Email", str)
+        self.assertTrue(without_validator.validate("string"))
+        self.assertTrue(without_validator.validate(""))
+
+        with_validator = Field("email", "Email", str, lambda s: "@" in s)
+        self.assertTrue(with_validator.validate("me@email.com"))
+        self.assertFalse(with_validator.validate(""))
+        
+
+if __name__ == "__main__":
+    unittest.main()
